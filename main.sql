6561462321698f455ea99dec7408468919c882b4
nichole-dayane06 <nichole.molinos@escola.pr.gov.br>
2025-08-15 09:15:30 -0300

    Add files via upload

---
+++ main.sql
@@ -0,0 +1,16 @@
+CREATE TABLE tab_paises (
+    id_pais INTEGER PRIMARY KEY NOT NULL,
+    nome_pais VARCHAR (40) NOT NULL
+);
+INSERT INTO tab_paises VALUES (
+    1,
+    'brasil'
+);
+INSERT INTO tab_paises VALUES (
+    2,
+    'italia'
+);
+INSERT INTO tab_paises VALUES (
+    3,
+    'japao'
+);
